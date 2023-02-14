9f2f32fe9cb103dd0ebfa1a2998bc936beace162
raquelgm88 <raquel.rgm7@gmail.com>
2023-02-14 10:11:35 +0100

    done HTML structure and started JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+
+//variables
+
+const button = document.querySelector('.js__button');
+const numberElement = document.querySelector('.js__number');
+const clueElement = document.querySelector('.js__clue');
+const counterElement = document.querySelector('.js__counter');
+let counter = 0;
+
+
+//funciones
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+
+
+//eventos
+function handleClickButton(event) {
+    event.preventDefault();
+
+}
+
+button.addEventListener('click', handleClickButton);
