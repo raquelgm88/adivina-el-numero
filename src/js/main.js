849286eae238300c9d5f76073c01d82da4dd588f
raquelgm88 <raquel.rgm7@gmail.com>
2023-02-22 21:16:07 +0100

    done issues

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@
 console.log('>> Ready :)');
 
 
-//variables
+//VARIABLES
 
 const button = document.querySelector('.js__button');
 const numberElement = document.querySelector('.js__input');
@@ -14,42 +14,57 @@ const counterElement = document.querySelector('.js__counter');
 let counter = 0;
 let randomNumber = 0;
 const max = 100;
-//funciones
+
+
+//FUNCIONES
+
+//Función para generar un número aleatorio
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
 
+//Almaceno en la variable randomNumber el número que se genera aleatoriamente
 randomNumber = getRandomNumber(max);
-console.log('Mi número aleatorio es ' + randomNumber);
 
+//Función para pintar en la consola el número aleatorio
+function randomNumberPainted () {
+  console.log('Mi número aleatorio es ' + randomNumber);
+}
+randomNumberPainted();
+
+//Función para extraer el número que escribe el usuario
 function getNumber() {
-    const userNumber = numberElement.value;
-    console.log(userNumber);
-    return userNumber;
+  const userNumber = parseInt(numberElement.value);
+  return userNumber;
 }
 
+//Función para pintar el HTML en el clueElement
+function clueText(clue) {
+  clueElement.innerHTML = clue;
+}
+
+//Función para comprobar el número que escribe el usuario
 function checkNumber(userNumber) {
-    if(userNumber > max) {
-        clueElement.innerHTML = 'El número debe estar entre 1 y ' + max;
-    }else if(userNumber < randomNumber) {
-        clueElement.innerHTML = 'Demasiado bajo';
-    }else if(userNumber == randomNumber) {
-        clueElement.innerHTML = '¡¡¡Has ganado campeona!!!';
-    }else if(userNumber > randomNumber) {
-        clueElement.innerHTML = 'Demasiado alto';
-     }
+  if (userNumber === '') {
+    clueText('Número de intentos: 0');
+  }else if(userNumber > max) {
+    clueText('El número debe estar entre 1 y ' + max);
+  }else if(userNumber < randomNumber) {
+    clueText('Demasiado bajo');
+  }else if(userNumber === randomNumber) {
+    clueText('¡¡¡Has ganado campeona!!!');
+  }else if(userNumber > randomNumber) {
+    clueText('Demasiado alto');
+  }
 }
 
-
-
-//eventos
+//EVENTO
 function handleClickButton(event) {
-    event.preventDefault();
-    const number = getNumber();
-    checkNumber(number);
-    counter++;
-    counterElement.innerHTML(counter);
+  event.preventDefault();
+  const number = getNumber();
+  checkNumber(number);
+  counter++;
+  counterElement.innerHTML = 'Número de intentos: ' + counter;
 }
-
 
 button.addEventListener('click', handleClickButton);
